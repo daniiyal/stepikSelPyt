463d5658ecb754c19349e2f3fc0abda0c09d8f9d
daniiyal <danik.magomedow@mail.ru>
2020-06-14 03:21:26 +0300

    solution added

--- StepikCourseTask/test_items.py
+++ StepikCourseTask/test_items.py
@@ -1,6 +1,6 @@
 import pytest
 import time
 
-def test_item(browser, language):
+def test_item(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
