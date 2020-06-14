4474c76debb19484d0584713e7c0c536b8c45bef
daniiyal <danik.magomedow@mail.ru>
2020-06-14 03:30:06 +0300

    refactoring

--- StepikTasks/conftest.py
+++ StepikTasks/conftest.py
@@ -2,12 +2,13 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default = None,  help="Choose browser: chrome or firefox")
+    parser.addoption('--browser_name', action='store', default = 'chrome',  help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope = 'function')
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
